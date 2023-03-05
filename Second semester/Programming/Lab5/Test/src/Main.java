8b3ac8dfcdf4c4d7fc3a4a7ca4461831e16eff3c
Timur <stt1xx@mail.ru>
2023-03-05 15:31:57 +0300

    add some data

---
+++ Second semester/Programming/Lab5/Test/src/Main.java
@@ -0,0 +1,18 @@
+import java.lang.reflect.Field;
+
+public class Main {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        int number = myClass.getNumber();
+        String name = null; // no getter
+        System.out.println(number + name);
+        try{
+            Field field = myClass.getClass().getDeclaredField("name");
+            field.setAccessible(true);
+            name = (String) field.get(myClass);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        System.out.println(number + name);
+    }
+}
